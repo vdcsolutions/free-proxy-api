3c3313dcee6b9009f487d240f75e934976b264fc
vdcsolutions <vdc.solutions@gmail.com>
2023-04-07 14:47:53 +0200

    fix

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-cd /app/http_proxy_list
-python main.py &
-sleep 5
+
 cd /app
-exec uvicorn api:app --host 0.0.0.0 --port 8000 --workers 4 --log-level info --proxy-headers
+exec uvicorn api:app --host 0.0.0.0 --port 8000 --workers 4 --log-level info --proxy-headers &
+
+cd /app/http_proxy_list
+while true
+do
+    echo "Updating proxy list..."
+    python main.py
+    sleep 300
+done
