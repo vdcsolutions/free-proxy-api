d1a8dbf1acb993a9e0c967b180d907d1b8c1ea61
vdcsolutions <vdc.solutions@gmail.com>
2023-04-07 15:31:50 +0200

    fix

--- run.sh
+++ run.sh
@@ -3,13 +3,15 @@
 cd /app
 exec uvicorn api:app --host 0.0.0.0 --port 8000 --workers 4 --log-level info --proxy-headers &
 
-cd /app/http_proxy_list
+
 while true
 do
+    cd /app/http_proxy_list
     echo "Deleting old data files..."
     rm proxy-list/data.txt proxy-list/data.json
     echo "Updating proxy list..."
     python main.py
-    python ../add_timestamp.py
+    cd /app
+    python add_timestamp.py
     sleep 300
 done
