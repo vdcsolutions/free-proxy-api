e5acedbad96c19a7c3dfb291cbc840167c70d041
vdcsolutions <vdc.solutions@gmail.com>
2023-04-07 13:42:47 +0200

    fix

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+source /opt/venv/bin/activate
 python /app/http_proxy_list/main.py &
 sleep 5
 exec /opt/venv/bin/uvicorn api:app --host 0.0.0.0 --port 8000 --workers 4 --log-level info --proxy-headers
