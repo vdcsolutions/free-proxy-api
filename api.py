9ccf5e903b25d33a56b1b850e724ff5ee96c2faa
vdcsolutions <vdc.solutions@gmail.com>
2023-04-07 14:38:46 +0200

    fix

--- api.py
+++ api.py
@@ -2,24 +2,20 @@ from fastapi import FastAPI
 import datetime
 import json
 import os
-import sys
-
-def get_script_path():
-    return os.path.dirname(os.path.realpath(sys.argv[0]))
 
 app = FastAPI()
 
 
-print(os.path.dirname(os.path.realpath(__file__)))
 @app.get("/proxy-list")
 async def get_json_list():
-    print(get_script_path())
     relative_path = 'http-proxy-list/proxy-list/data-with-geolocation.json'
     absolute_path = os.path.abspath(relative_path)
+    print(absolutu_path)
+    print(relative_path)
     timestamp = os.path.getmtime(absolute_path)
     last_modified = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
 
-    with open(relative_path, 'r') as f:
+    with open(absolute_path, 'r') as f:
         json_data = json.load(f)
         for element in json_data:
             element['updated_at'] = last_modified
