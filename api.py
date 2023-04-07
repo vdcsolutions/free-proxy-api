1b9926ae8917c0af3f1a01b23575f64042c38628
vdcsolutions <vdc.solutions@gmail.com>
2023-04-07 14:41:55 +0200

    fix

--- api.py
+++ api.py
@@ -8,9 +8,9 @@ app = FastAPI()
 
 @app.get("/proxy-list")
 async def get_json_list():
-    relative_path = 'http-proxy-list/proxy-list/data-with-geolocation.json'
+    relative_path = 'http_proxy_list/proxy-list/data-with-geolocation.json'
     absolute_path = os.path.abspath(relative_path)
-    print(absolutu_path)
+    print(absolute_path)
     print(relative_path)
     timestamp = os.path.getmtime(absolute_path)
     last_modified = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
