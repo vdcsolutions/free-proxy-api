fbe7cf5cfd36f58e537b5cd7293578ec19beb4ec
vdcsolutions <vdc.solutions@gmail.com>
2023-04-07 15:46:16 +0200

    fix

--- run.sh
+++ run.sh
@@ -2,14 +2,14 @@
 
 cd /app
 exec uvicorn api:app --host 0.0.0.0 --port 8000 --workers 4 --log-level info --proxy-headers &
-
+python add_timestamp.py
 
 while true
 do
     cd /app
-    python add_timestamp.py
     cd /app/http_proxy_list
     rm proxy-list/data.txt proxy-list/data.json
     python main.py >/dev/null 2>&1
+    python add_timestamp.py
     sleep 300
 done
