1ae3c362838765e82bfe392e5e39f096d0184a70
vdcsolutions <86836806+vdcsolutions@users.noreply.github.com>
2023-04-11 01:31:14 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#cd /app
+cd /app
 exec uvicorn api:app --host 0.0.0.0 --port 8000 --workers 4 --log-level info --proxy-headers &
 echo '[  {    "message": "JUST WOKE UP! I AM COOKING FIRST BATCH OF DELICIOUS FREE PROXIES FOR YOU RIGHT NOW, SO CHECK AGAIN IN FEW MINUTES!"  }]' > http_proxy_list/proxy-list/data-with-geolocation.json
 
@@ -8,12 +8,10 @@ while true
 do
     timestamp=$(date +"%Y-%m-%d %H:%M:%S")
     echo "$timestamp: Starting update process"
-    #cd /app/
-    cd http_proxy_list
+    cd /app/http_proxy_list
     echo "$timestamp: Fetching new proxy data"
     python main.py >/dev/null 2>&1
-    #cd /app
-    cd ..
+    cd /app
     echo "$timestamp: Updating timestamp and deleting old entries"
     echo "$timestamp: Updating timestamp and deleting old entries"
     python update_data.py --filepath http_proxy_list/proxy-list/data-with-geolocation.json
