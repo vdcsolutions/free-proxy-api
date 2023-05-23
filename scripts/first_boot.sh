9af6de7552ed815d8d12f05dbb6070e8afdb0d72
vdcsolutions <vdc.solutions@gmail.com>
2023-05-23 16:22:30 +0200

    created first_boot, startup_routine and deploy scripts

--- scripts/first_boot.sh
+++ scripts/first_boot.sh
@@ -8,17 +8,20 @@ apt-get install -y docker.io
 apt-get install -y ufw
 
 # Allow ports in UFW
+ufw allow 20
+ufw allow 20/tcp
 ufw allow 80
 ufw allow 80/tcp
 ufw allow 8000
 ufw allow 8000/tcp
 
+# Move startup_routine.sh to /etc/init.d/
+cp startup_routine.sh /etc/init.d/
+chmod +x /etc/init.d/startup_routine.sh
+
+
 # Enable UFW at boot
 ufw enable
-
-# Move startup_routine.sh to /etc/init.d/
-mv startup_routine.sh /etc/init.d/
-chmod +x /etc/init.d/startup_routine.sh
 
 # Reboot the server
 reboot
